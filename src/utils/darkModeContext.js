d3f0c75e0ca4b8b1ea75f8db699abeb9235a2641
ManasaNagaram <nagarammanasa235@gmail.com>
2025-03-18 21:17:24 +0530

    change default theme

--- src/utils/darkModeContext.js
+++ src/utils/darkModeContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
-    const [darkMode, setDarkMode] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
 
     return (
         <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
