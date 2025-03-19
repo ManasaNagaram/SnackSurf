6629c05431ce029d8b31d0499564089a04caf359
ManasaNagaram <nagarammanasa235@gmail.com>
2025-03-19 18:01:58 +0530

    cors proxy

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -7,7 +7,7 @@ const useResTrauntMenu=  (resId)=>{
         fetchMenu();
     },[]);
     const fetchMenu = async () => { 
-        const data = await fetch(MENU_API+resId);
+        const data = await fetch(`${MENU_API}${resId}`);
         const json = await data.json();
         setResInfo(json.data);
       };
